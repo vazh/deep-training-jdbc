97795cd1b9b1a6e10cfe391b3db7b9a3036f1981
Muhammad Hariz Faza <hariz.faza@asyst.co.id>
2024-02-23 13:49:03 +0700

    init(*)

---
+++ src/main/java/id/co/asyst/deep/training/jdbc/JdbcApplication.java
@@ -0,0 +1,13 @@
+package id.co.asyst.deep.training.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/asyst/deep/training/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.asyst.deep.training.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
